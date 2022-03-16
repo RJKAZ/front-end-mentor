476a492e3f36b54c9fa715beac2712ef70ac6b26
unknown <rjkaz@comcast.net>
2022-03-15 20:41:24 -0400

    project 15 javascript

--- newbie-challenges/15-advice-generator/solution/script.js
+++ newbie-challenges/15-advice-generator/solution/script.js
@@ -1,1 +1,21 @@
-//https://github1s.com/TheShonuff/advice-generator/blob/HEAD/index.html#L40
+const adviceID = document.querySelector(".adviceID");
+const advice = document.querySelector(".advice");
+
+async function fetchAdvice() {
+  let response = await fetch("https://api.adviceslip.com/advice", {
+    cache: "no-store",
+  });
+
+  console.log(response.status);
+  console.log(response.statusText);
+
+  if (response.status === 200) {
+    let data = await response.json();
+    console.log(data.slip.advice);
+    console.log(data.slip.id);
+
+    adviceID.innerHTML = `<h1>ADVICE #${data.slip.id}<h1>`;
+
+    advice.innerHTML = `<blockquote>"${data.slip.advice}"</blockquote>`;
+  }
+}
