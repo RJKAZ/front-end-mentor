d4703c0cd61d577a21e98d4ee129c6b226ae303a
reese <rjkaz@comcast.net>
2022-04-09 14:10:49 -0400

    update

--- newbie-challenges/16-interactive-rating-component/solution/script.js
+++ newbie-challenges/16-interactive-rating-component/solution/script.js
@@ -1,1 +1,28 @@
 // https://github1s.com/DummyKen/interactive-rating-component/blob/HEAD/index.html
+
+// all the selectors
+
+const submitBtn = document.querySelector('#submit');
+const ratings = document.querySelector('.numbers');
+const rating = document.querySelector('.number');
+const ratingValue = document.querySelector('.rValue');
+const ratingState = document.querySelector('.rating');
+const thankYouState = document.querySelector('.thank-you');
+const backBtn = document.querySelector('.back');
+let value = 0;
+
+submitBtn.addEventListener('click', () => {
+  ratingState.getElementsByClassName.display = 'none';
+  thankYouState.getElementsByClassName.display = 'block';
+});
+
+backBtn.addEventListener('click', () => {
+  ratingState.style.display = 'block';
+  thankYouState.style.display = 'none';
+});
+
+ratings.addEventListener('click', (e) => {
+  e.target.classList.add('selected');
+  value = e.target.innerText;
+  ratingValue.textContent = value;
+});
